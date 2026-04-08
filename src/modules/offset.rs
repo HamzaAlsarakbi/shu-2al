c546064e80cbbbc7bc5b889f8627320765b45ab7
Hamza Alsarakbi <hamzaalsarakbi@gmail.com>
2026-04-08 01:54:23 -0400

    feat: added offset tests

--- src/modules/offset.rs
+++ src/modules/offset.rs
@@ -10,9 +10,11 @@ use crate::{
 
 /// Offset module settings
 pub struct OffsetModule {
+    /// Whether the module is enabled or not.
     enabled: bool,
-    /// The offset in milliseconds to be applied to the subtitle timings.
+    /// Time offset to apply to the subtitle timings.
     offset: Duration,
+    /// Direction of the offset (forward or backward).
     direction: Direction,
 }
 
@@ -41,5 +43,67 @@ impl Module for OffsetModule {
         }
         drop(lock);
         Ok(input)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        sync::{Arc, Mutex},
+        time::Duration,
+    };
+
+    use crate::{
+        core::{direction::Direction, srt::SRT, subtitle::Subtitle, timestamp::Timestamp},
+        modules::{module::Module, offset::OffsetModule},
+    };
+
+    #[test]
+    fn test_offset_module() {
+        let subtitles = vec![
+            Subtitle {
+                index: 1,
+                start_time: Timestamp::from_string("00:00:01,000").unwrap(),
+                end_time: Timestamp::from_string("00:00:05,000").unwrap(),
+                text: "Hello, World!".to_string(),
+            },
+            Subtitle {
+                index: 2,
+                start_time: Timestamp::from_string("00:00:06,000").unwrap(),
+                end_time: Timestamp::from_string("00:00:10,000").unwrap(),
+                text: "This is a test.".to_string(),
+            },
+        ];
+
+        let srt = SRT {
+            subtitles: subtitles.clone(),
+            file_path: "test.srt".to_string(),
+        };
+
+        let offset_module = OffsetModule {
+            enabled: true,
+            offset: Duration::from_secs(2),
+            direction: Direction::Forward,
+        };
+
+        let input = Arc::new(Mutex::new(srt));
+        let result = offset_module.process(input.clone()).unwrap();
+
+        assert_eq!(
+            result.lock().unwrap().subtitles[0].start_time,
+            Timestamp::from_string("00:00:03,000").unwrap()
+        );
+        assert_eq!(
+            result.lock().unwrap().subtitles[0].end_time,
+            Timestamp::from_string("00:00:07,000").unwrap()
+        );
+        assert_eq!(
+            result.lock().unwrap().subtitles[1].start_time,
+            Timestamp::from_string("00:00:08,000").unwrap()
+        );
+        assert_eq!(
+            result.lock().unwrap().subtitles[1].end_time,
+            Timestamp::from_string("00:00:12,000").unwrap()
+        );
     }
 }
